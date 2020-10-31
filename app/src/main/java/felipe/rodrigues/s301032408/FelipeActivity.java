55ee5de9ef4d42c28491073de65b816a180ca9ba
feliperodrigues <beraldo.felipe@gmail.com>
2020-10-31 08:42:58 -0400

    Initial commit

---
+++ app/src/main/java/felipe/rodrigues/s301032408/FelipeActivity.java
@@ -0,0 +1,15 @@
+package felipe.rodrigues.s301032408;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Felipe Rodrigues 301032408
+
+public class FelipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
